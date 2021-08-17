3398bda67992590124b411225fa5983fe0f52752
aleveha <nikita.tashilin@gmail.com>
2021-08-17 22:33:20 +0200

    edit news table in DB and prisma schema

--- src/Query/newsQueries.ts
+++ src/Query/newsQueries.ts
@@ -12,13 +12,15 @@ export const getAllNews = async (): Promise<news[]> => {
 };
 
 export const addNews = async (body: news): Promise<news> => {
-    const { date, name, content } = body;
+    const { date, name_cz, name_en, content_cz, content_en } = body;
 
     return await prisma.news.create({
         data: {
             date: new Date(date),
-            name: name,
-            content: content,
+            name_cz: name_cz,
+            name_en: name_en,
+            content_cz: content_cz,
+            content_en: content_en,
         },
     });
 };
